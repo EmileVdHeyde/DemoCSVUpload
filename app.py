deeaed87bb330f407386ce32b2f168d73f265f5b
Emile Van Der Heyde <emilevdheyde@gmail.com>
2020-06-13 21:24:26 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+
+import os
+from flask import render_template, request, redirect, url_for
+
+
+app = Flask(__name__)
+
+@application.route('upload.html',methods = ['POST'])
+def upload_route_summary():
+    if request.method == 'POST':
+  # Create variable for uploaded file
+     f = request.files['fileupload']
+ #store the file contents as a string
+     fstring = f.read()
+ #create list of dictionaries keyed by header row
+     csv_dicts = [{k: v for k, v in row.items()} for row in
+    csv.DictReader(fstring.splitlines(), skipinitialspace=True)]
+ #do something list of dictionaries
+     return ("success")
+
+if __name__ == "__main__":
+    app.run(port=4555, debug=True)
