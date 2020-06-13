39cf6237a99090819023bb9851f27ed08fe8cd7f
Emile Van Der Heyde <emilevdheyde@gmail.com>
2020-06-13 21:38:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
 
-import os
-from flask import render_template, request, redirect, url_for
-
-
-app = Flask(__name__)
-
 @application.route('upload.html',methods = ['POST'])
 def upload_route_summary():
     if request.method == 'POST':
@@ -14,9 +8,6 @@ def upload_route_summary():
      fstring = f.read()
  #create list of dictionaries keyed by header row
      csv_dicts = [{k: v for k, v in row.items()} for row in
-    csv.DictReader(fstring.splitlines(), skipinitialspace=True)]
+     csv.DictReader(fstring.splitlines(), skipinitialspace=True)]
  #do something list of dictionaries
      return ("success")
-
-if __name__ == "__main__":
-    app.run(port=4555, debug=True)
